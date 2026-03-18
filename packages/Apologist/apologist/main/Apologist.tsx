82ca90eb2d576f60ac3c53af077707e579c10ba9
Kallyn Gowdy <kallyngowdy@gmail.com>
2026-03-18 11:59:22 -0400

    fix: Include referrerpolicy in the youtube iframe

--- packages/Apologist/apologist/main/Apologist.tsx
+++ packages/Apologist/apologist/main/Apologist.tsx
@@ -271,6 +271,7 @@ function SgCard({
             src={videoSrc}
             title={item.title || "YouTube video"}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
             allowFullScreen
             onError={() => setVideoError(true)}
           />
