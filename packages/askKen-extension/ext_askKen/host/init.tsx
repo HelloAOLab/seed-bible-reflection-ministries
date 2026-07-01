18f567505965ceba64a84a2bd3713fd1caf9f4ca
Amit Pant <amitpant@Amits-MacBook-Air.local>
2026-07-01 21:33:45 +0530

    add askken in versetoolbar

--- packages/askKen-extension/ext_askKen/host/init.tsx
+++ packages/askKen-extension/ext_askKen/host/init.tsx
@@ -28,5 +28,26 @@ registerExtension({
       },
       priority: 100,
     });
+    yield context.tools.registerVerseToolbarTool({
+      id: "ext_askKen",
+      title: {
+        key: "askKen",
+        defaultValue: "askKen",
+        ns: "ext_askKen",
+      },
+      icon: () => <span className="material-symbols-outlined">chat</span>,
+      onSelect: () => {
+        context.panes.openPane({
+          type: "detached",
+          detachedAnchor: "side",
+          component: () => {
+            const state = useMemo(() => createAskKenState(context), []);
+
+            return <AskKen state={state} />;
+          },
+        });
+      },
+      priority: 100,
+    });
   },
 });
