9f38982ff795ecf75439a2cb4abf925ca6e6c6c6
Amit Pant <amitpant@Amits-MacBook-Air.local>
2026-08-28 00:16:29 +0530

    fix

--- packages/discovery-extension/ext_discovery/host/managers/index.tsx
+++
@@ -1,3 +0,0 @@
-export * from "./ApologistCardManager";
-export * from "./ApologistManager";
-export * from "./ApologistPanelManager";
--- packages/discovery-extension/index.ts
+++
@@ -1,1 +0,0 @@
-export { default } from "./ext_discovery/host/init";
